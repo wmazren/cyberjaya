8ff1fb92e39ba6e0584b82f984671a448a88a88a
wmazren <w.m.azren@gmail.com>
2012-12-24 23:15:19 +0800

    generate users controller

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,5 @@
 class Organization < ActiveRecord::Base
+  has_many :users
+
   attr_accessible :address1, :address2, :city, :country, :fax, :name, :phone, :postcode, :state
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  belongs_to :organization
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -7,5 +9,5 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
-  
+
 end
