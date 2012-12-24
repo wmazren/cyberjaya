740b918e8abb0cac8960092e37e4a0a4e7799ffc
wmazren <w.m.azren@gmail.com>
2012-12-24 23:31:08 +0800

    edit Users mvc

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Cyberjaya::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :path_prefix => 'd'
+  resources :users
+
+  resources :organizations
 
   root :to => 'home#index'
 end
