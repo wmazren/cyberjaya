b504bae8447d0a6d99a9bc74fdc1b5cdbd12055c
wmazren <w.m.azren@gmail.com>
2012-12-24 22:40:11 +0800

    remove name from Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121224135712) do
+ActiveRecord::Schema.define(:version => 20121224143914) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(:version => 20121224135712) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
