67385bcbe06bb9c369f2eea320cab589bac85f91
wmazren <w.m.azren@gmail.com>
2012-12-24 21:52:37 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cyberjaya::Application.config.secret_token = '42187d20720a9ab5ee0cb96afb3acecbef16973e1e5faa548f898081372e92c17988ad1169a7ef5578f462de8df7c177d3e132dc75d1f3c227feaea394c5f43a'
